ec9478578cd34a29e6e61dce2ce691b8bf4bb55f
Oleg <mbeta75@gmail.com>
2020-09-16 13:04:59 +0300

    added zoom, keyListener, setting and count size point

--- opengl_computer_graphics_1_lab/src/main/java/com/opengl/Main.java
+++ opengl_computer_graphics_1_lab/src/main/java/com/opengl/Main.java
@@ -2,40 +2,61 @@ package com.opengl;
 
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.fixedfunc.GLMatrixFunc;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyListener;
 import java.util.List;
 
 //9
 // (-W/5, -H/2.5) (-W/5, H/2.5) 1 Сірий
 // (-W/4, -H/4) (W/4, 0) 3 Зелений
 
-public class Main implements GLEventListener {
+public class Main implements GLEventListener, KeyListener {
 
     public static final int windowWidth = 800;
     public static final int windowHeight = 600;
     public static final String windowTitle = "Computer graphics. Laboratory work #1";
+    private static final char PLUS = '+';
+    private static final char MINUS = '-';
+
+    private GL2 gl;
+    private final GLCanvas gc;
+
+    private float zoomX = 1f, zoomY = 1f, zoomZ = 1f;
+
+    public Main(GLCanvas gc) {
+        this.gc = gc;
+    }
 
     public static void main(String[] args) {
         final GLProfile gp = GLProfile.get(GLProfile.GL2);
         GLCapabilities cap = new GLCapabilities(gp);
 
         final GLCanvas gc = new GLCanvas(cap);
-        Main main = new Main();
+
+        Main main = new Main(gc);
+
         gc.addGLEventListener(main);
+        gc.addKeyListener(main);
         gc.setSize(windowWidth, windowHeight);
 
         final JFrame frame = new JFrame(windowTitle);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(gc);
+        frame.addKeyListener(main);
         frame.setSize(windowWidth, windowHeight);
         frame.pack();
         frame.setVisible(true);
     }
 
     public void init(GLAutoDrawable glAutoDrawable) {
-
+        final GL2 gl = glAutoDrawable.getGL().getGL2();
+        this.gl = gl;
+        gl.glShadeModel(GL.GL_LINE_SMOOTH);
+        gl.glEnable(GL2.GL_MULTISAMPLE);
+        gl.glMatrixMode(GLMatrixFunc.GL_MODELVIEW);
     }
 
     public void dispose(GLAutoDrawable glAutoDrawable) {
@@ -43,7 +64,12 @@ public class Main implements GLEventListener {
     }
 
     public void display(GLAutoDrawable glAutoDrawable) {
-        final GL2 gl = glAutoDrawable.getGL().getGL2();
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+
+        gl.glScalef(zoomX,zoomY,zoomZ);
+
+        gl.glEnable(GL.GL_MULTISAMPLE);
+
         // line 1
         Point line_1_start = new Point();
         line_1_start.setX((windowWidth/-5.0)/1000);
@@ -53,7 +79,7 @@ public class Main implements GLEventListener {
         line_1_end.setX((windowWidth/-5.0)/1000);
         line_1_end.setY((windowHeight/2.5)/1000);
 
-        drawLine(gl, line_1_start, line_1_end, Colors.GREEN);
+        drawLine(gl, line_1_start, line_1_end, Colors.YELLOW);
 
         // line 2
         Point line_2_start = new Point();
@@ -66,11 +92,16 @@ public class Main implements GLEventListener {
 
         drawLine(gl, line_2_start, line_2_end, Colors.RED);
 
-        List<Point> points1 = Point.generatePoints(line_1_start, line_1_end, 10);
-        drawPoints(gl, points1, 4f, Colors.ORANGE);
+        final int steps = 9;
+        final float sizePoint = 1f;
 
-        List<Point> points2 = Point.generatePoints(line_2_start, line_2_end, 10);
-        drawPoints(gl, points2, 4f, Colors.YELLOW);
+        List<Point> points1 = Point.generatePoints(line_1_start, line_1_end, steps);
+        drawPoints(gl, points1, sizePoint, steps, Colors.GRAY);
+
+        List<Point> points2 = Point.generatePoints(line_2_start, line_2_end, steps);
+        drawPoints(gl, points2, sizePoint, steps, Colors.GREEN);
+
+        gl.glFlush();
     }
 
     public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
@@ -80,9 +111,17 @@ public class Main implements GLEventListener {
     private void drawLine(GL2 gl, Point start, Point end, Colors c) {
         gl.glBegin(GL2.GL_LINES);
         gl.glColor3f(c.getRed(), c.getGreen(), c.getBlue());
-        gl.glVertex2d(start.getX(), start.getY());
-        gl.glVertex2d(end.getX(), end.getY());
+        gl.glVertex3d(start.getX(), start.getY(), 0);
+        gl.glVertex3d(end.getX(), end.getY(), 0);
         gl.glEnd();
+    }
+
+    private void drawPoints(final GL2 gl, List<Point> points, float pointSize, int pointSizeStep, Colors c) {
+        for(int i = 1; i <= pointSizeStep; i++) {
+            drawPoint(gl, points.get(i), pointSize * i, c);
+        }
+
+        return;
     }
 
     private void drawPoints(final GL2 gl, List<Point> points, float pointSize, Colors c) {
@@ -94,7 +133,41 @@ public class Main implements GLEventListener {
         gl.glColor3f(c.getRed(), c.getGreen(), c.getBlue());
         gl.glPointSize(pointSize);
         gl.glBegin(GL2.GL_POINTS);
-        gl.glVertex2d(point.getX(),point.getY());
+        gl.glVertex3d(point.getX(),point.getY(), 0);
         gl.glEnd();
+    }
+
+    @Override
+    public void keyTyped(java.awt.event.KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(java.awt.event.KeyEvent keyEvent) {
+        if(PLUS == keyEvent.getKeyChar()) {
+            zoomPlus();
+        } else if(MINUS == keyEvent.getKeyChar()) {
+            zoomMinus();
+        }
+
+        gc.repaint();
+    }
+
+    private void zoomMinus() {
+        zoomX -= 0.1f;
+        zoomY -= 0.1f;
+        System.out.println("minus " + zoomX + " " + zoomY + " " + zoomZ);
+
+    }
+
+    private void zoomPlus() {
+        zoomX += 0.1f;
+        zoomY += 0.1f;
+        System.out.println("plus " + zoomX + " " + zoomY + " " + zoomZ);
+    }
+
+    @Override
+    public void keyReleased(java.awt.event.KeyEvent keyEvent) {
+
     }
 }
