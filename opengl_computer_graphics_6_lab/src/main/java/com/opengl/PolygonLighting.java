681702932d7bb71c42d0bda099a78e43c002edd4
Oleg <mbeta75@gmail.com>
2020-11-08 18:13:13 +0200

    fix figure in 6 lab

--- opengl_computer_graphics_6_lab/src/main/java/com/opengl/PolygonLighting.java
+++ opengl_computer_graphics_6_lab/src/main/java/com/opengl/PolygonLighting.java
@@ -4,10 +4,12 @@ import javax.swing.JFrame;
 
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.FPSAnimator;
 
 public class PolygonLighting implements GLEventListener {
-    private float rpoly;
+    private float rquad = 0.0f;
+    private GLU glu = new GLU();
 
     @Override
 
@@ -20,24 +22,54 @@ public class PolygonLighting implements GLEventListener {
         gl.glClear( GL2.GL_COLOR_BUFFER_BIT |
                 GL2.GL_DEPTH_BUFFER_BIT );
         gl.glLoadIdentity();       // Reset The View
-        gl.glRotatef( rpoly, 0.0f, 1.0f, 0.0f );
 
-        gl.glBegin( GL2.GL_POLYGON );
+        gl.glTranslatef( 0f, 0f, -5.0f );
 
-        gl.glVertex3f( 0f,0.5f,0f );
-        gl.glVertex3f( -0.5f,0.2f,0f );
-        gl.glVertex3f( -0.5f,-0.2f,0f );
-        gl.glVertex3f( 0f,-0.5f,0f );
-        gl.glVertex3f( 0f,0.5f,0f );
-        gl.glVertex3f( 0.5f,0.2f,0f );
-        gl.glVertex3f( 0.5f,-0.2f,0f );
-        gl.glVertex3f( 0f,-0.5f,0f );
+        // Rotate The Cube On X, Y & Z
+        gl.glRotatef(rquad, 1.0f, 1.0f, 1.0f);
 
-        gl.glEnd();
+        //giving different colors to different sides
+        gl.glBegin(GL2.GL_QUADS); // Start Drawing The Cube
+        gl.glColor3f(1f,0f,0f); //red color
+        gl.glVertex3f(1.0f, 1.0f, -1.0f); // Top Right Of The Quad (Top)
+        gl.glVertex3f( -1.0f, 1.0f, -1.0f); // Top Left Of The Quad (Top)
+        gl.glVertex3f( -1.0f, 1.0f, 1.0f ); // Bottom Left Of The Quad (Top)
+        gl.glVertex3f( 1.0f, 1.0f, 1.0f ); // Bottom Right Of The Quad (Top)
+
+        gl.glColor3f( 0f,1f,0f ); //green color
+        gl.glVertex3f( 1.0f, -1.0f, 1.0f ); // Top Right Of The Quad
+        gl.glVertex3f( -1.0f, -1.0f, 1.0f ); // Top Left Of The Quad
+        gl.glVertex3f( -1.0f, -1.0f, -1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( 1.0f, -1.0f, -1.0f ); // Bottom Right Of The Quad
+
+        gl.glColor3f( 0f,0f,1f ); //blue color
+        gl.glVertex3f( 1.0f, 1.0f, 1.0f ); // Top Right Of The Quad (Front)
+        gl.glVertex3f( -1.0f, 1.0f, 1.0f ); // Top Left Of The Quad (Front)
+        gl.glVertex3f( -1.0f, -1.0f, 1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( 1.0f, -1.0f, 1.0f ); // Bottom Right Of The Quad
+
+        gl.glColor3f( 1f,1f,0f ); //yellow (red + green)
+        gl.glVertex3f( 1.0f, -1.0f, -1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( -1.0f, -1.0f, -1.0f ); // Bottom Right Of The Quad
+        gl.glVertex3f( -1.0f, 1.0f, -1.0f ); // Top Right Of The Quad (Back)
+        gl.glVertex3f( 1.0f, 1.0f, -1.0f ); // Top Left Of The Quad (Back)
+
+        gl.glColor3f( 1f,0f,1f ); //purple (red + green)
+        gl.glVertex3f( -1.0f, 1.0f, 1.0f ); // Top Right Of The Quad (Left)
+        gl.glVertex3f( -1.0f, 1.0f, -1.0f ); // Top Left Of The Quad (Left)
+        gl.glVertex3f( -1.0f, -1.0f, -1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( -1.0f, -1.0f, 1.0f ); // Bottom Right Of The Quad
+
+        gl.glColor3f( 0f,1f, 1f ); //sky blue (blue +green)
+        gl.glVertex3f( 1.0f, 1.0f, -1.0f ); // Top Right Of The Quad (Right)
+        gl.glVertex3f( 1.0f, 1.0f, 1.0f ); // Top Left Of The Quad
+        gl.glVertex3f( 1.0f, -1.0f, 1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( 1.0f, -1.0f, -1.0f ); // Bottom Right Of The Quad
+        gl.glEnd(); // Done Drawing The Quad
 
         gl.glFlush();
 
-        rpoly += 0.2f;  //assigning the angle
+        rquad -= 0.15f;
 
         gl.glEnable( GL2.GL_LIGHTING );
         gl.glEnable( GL2.GL_LIGHT0 );
@@ -64,7 +96,20 @@ public class PolygonLighting implements GLEventListener {
 
     @Override
     public void reshape( GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4 ) {
-        // method body
+        final GL2 gl = arg0.getGL().getGL2();
+
+        if( arg4 == 0 ) {
+            arg4 = 1;
+        }
+
+        final float h = ( float ) arg3 / ( float ) arg4;
+        gl.glViewport( 0, 0, arg3, arg4 );
+        gl.glMatrixMode( GL2.GL_PROJECTION );
+        gl.glLoadIdentity();
+
+        glu.gluPerspective( 45.0f, h, 1.0, 20.0 );
+        gl.glMatrixMode( GL2.GL_MODELVIEW );
+        gl.glLoadIdentity();
     }
 
     public static void main( String[] args ) {
